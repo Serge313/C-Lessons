ddd12119244bdbaac037abb59cfc05d41d4a950a
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-08-04 05:11:30 +0300

    Example 002 modified

---
+++ Example002_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "настя")
+{
+    Console.WriteLine("Привет, Настя!");
+}
+else
+{
+    Console.Write("Здравствуй, ");
+    Console.WriteLine(username);
+}
